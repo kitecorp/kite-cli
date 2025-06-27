e7de680539c6714062041816fac38d7307a25354
Tudor Pop <tudor14pop@gmail.com>
2025-06-27 23:20:45 +0300

    Remove initResource

---
+++ src/main/java/io/zmeu/cli/Main.java
@@ -0,0 +1,39 @@
+package io.zmeu.cli;
+
+import io.zmeu.Zmeu;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.io.File;
+
+import static picocli.CommandLine.Parameters;
+
+@Command(name = "cio", mixinStandardHelpOptions = true, version = "cio 1.0",
+        description = "Prints the checksum (SHA-256 by default) of a file to STDOUT.")
+@Log4j2
+class Main implements Runnable {
+
+    @Parameters(paramLabel = "SourceFiles", description = "The source file")
+    private File[] sourceFiles;
+
+    private final Zmeu zmeu;
+
+
+    @SneakyThrows
+    public Main() {
+        zmeu = new Zmeu();
+    }
+
+    @SneakyThrows
+    @Override
+    public void run() { // your business logic goes here...
+        zmeu.run();
+    }
+
+    public static void main(String... args) {
+        var commandLine = new CommandLine(new Main());
+        commandLine.execute(args);
+    }
+}
